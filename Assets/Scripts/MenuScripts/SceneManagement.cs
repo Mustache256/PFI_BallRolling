99710f754bdc32d80c7e98818fbaeba05a252ce3
Mustache256 <71705907+Mustache256@users.noreply.github.com>
2021-12-28 17:03:39 +0000

    Completed Menu Functionality Except Audio

---
+++ Assets/Scripts/MenuScripts/SceneManagement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+  public void tutorialScene()
+  {
+    Debug.Log("Tutorial Scene Loaded");
+    SceneManager.LoadScene("MiniGame");
+  }
+  public void level1Scene()
+  {
+    Debug.Log("Level 1 Scene Loaded");
+    SceneManager.LoadScene("MiniGame");
+  }
+  public void level2Scene()
+  {
+    Debug.Log("Level 2 Scene Loaded");
+    SceneManager.LoadScene("MiniGame");
+  }
+  public void level3Scene()
+  {
+    Debug.Log("Level 3 Scene Loaded");
+    SceneManager.LoadScene("MiniGame");
+  }
+  public void level4Scene()
+  {
+    Debug.Log("Level 4 Scene Loaded");
+    SceneManager.LoadScene("MiniGame");
+  }
+}
