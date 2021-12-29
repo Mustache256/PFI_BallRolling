185bf2230664535f5dfff591ac9645c5fd82bf76
Mustache256 <71705907+Mustache256@users.noreply.github.com>
2021-12-29 16:53:05 +0000

    Minor menu adjustments and beginning of framework for camera movement
    Setting up the menu to be polished up with texturing and art to better fit the theme of the game, as well as beginning to create the framework to allow the camera to move in such a way as to create the illusion of the level moving around the player

---
+++ Assets/Scripts/GameScripts/Camera/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	private PlayerController player;
+
+	private float initialXRotation;
+
+	private float horizontalTilt;
+	private float verticalTilt;
+
+	private Transform mainCamera;
+	private Vector3 offset;
+
+	void Start () 
+	{
+		mainCamera = transform.GetChild(0);
+		player = FindObjectOfType<PlayerController>();
+		offset = transform.position;
+
+		initialXRotation = transform.eulerAngles.x;
+	}
+	
+	void Update () 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/GameScripts/Player/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public enum STATE
+  {
+		WAIT,
+		NORMAL,
+		DEAD,
+		VICTORY,
+		STATES
+  }
+
+	public float speed;
+  public Rigidbody orientation;
+  public Rigidbody spawnPoint;
+  public float maxSpeed;
+	public STATE currentState;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+
+		currentState = STATE.WAIT;
+	}
+	
+	void Update ()
+	{
+
+    float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+	  rb.AddForce (movement * speed * Time.deltaTime);
+
+    if(rb.velocity.magnitude > maxSpeed)
+    {
+      rb.velocity = rb.velocity.normalized * maxSpeed;
+    }
+
+    orientation.transform.position = rb.position;
+  }
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+		}
+		else if (other.gameObject.tag == "DontPickUp")
+		{
+			other.gameObject.SetActive(false);
+		}
+		else if (other.gameObject.tag == "DeathBox")
+		{
+			rb.transform.position = spawnPoint.position;
+		}
+	}
+
+	public bool OnGround()
+  {
+		return true; //Physics.CheckSphere(transform.position - (Vector3.up * 0.5f), groundCheckRadius, whatIsGround)
+  }
+}
