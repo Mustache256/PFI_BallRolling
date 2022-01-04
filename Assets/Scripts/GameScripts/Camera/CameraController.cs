bb5db0c29e327caf873fa04d68e2dcf7112570a2
Mustache256 <71705907+Mustache256@users.noreply.github.com>
2022-01-04 12:25:29 +0000

    Player and camera code setup

--- Assets/Scripts/GameScripts/Camera/CameraController.cs
+++ Assets/Scripts/GameScripts/Camera/CameraController.cs
@@ -3,27 +3,98 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour 
 {
+	private Transform mainCamera;
+
 	private PlayerController player;
 
 	private float initialXRotation;
-
 	private float horizontalTilt;
 	private float verticalTilt;
+	private float maxVerticalAngle;
+	private float maxHorizontalAngle;
+	private float tiltSpeed;
+	private float offset;
 
-	private Transform mainCamera;
-	private Vector3 offset;
+	private bool useFloorNormal;
 
 	void Start () 
 	{
 		mainCamera = transform.GetChild(0);
 		player = FindObjectOfType<PlayerController>();
-		offset = transform.position;
 
 		initialXRotation = transform.eulerAngles.x;
 	}
-	
-	void Update () 
+
+  void FixedUpdate()
+  {
+    if(player.currentState == PlayerController.STATE.NORMAL)
+    {
+			verticalTilt = Input.GetAxis("Vertical");
+			horizontalTilt = Input.GetAxis("Horizontal");
+
+			player.Move(verticalTilt, horizontalTilt, transform.right);
+    }
+  }
+  void Update () 
 	{
-		transform.position = player.transform.position + offset;
+		if (player.currentState == PlayerController.STATE.NORMAL)
+    {
+			CameraTilt();
+    }
 	}
+
+  void LateUpdate()
+  {
+    switch(player.currentState)
+    {
+			case PlayerController.STATE.WAIT:
+				StartStage();
+				break;
+			case PlayerController.STATE.NORMAL:
+				FollowTarget();
+				break;
+			case PlayerController.STATE.DEAD:
+				transform.LookAt(player.transform.position);
+				break;
+    }
+  }
+
+	void StartStage()
+  {
+		player.currentState = PlayerController.STATE.NORMAL;
+  }
+
+	void CameraTilt()
+  {
+		float scaledVerticalTilt = initialXRotation - (verticalTilt * maxVerticalAngle);
+
+		float angleBetweenFloorNormal = useFloorNormal ? Vector3.SignedAngle(Vector3.up, player.floorNormal, transform.right) : 0.0f;
+
+		Quaternion targetXRotation = Quaternion.Euler(scaledVerticalTilt + angleBetweenFloorNormal, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetXRotation, tiltSpeed * Time.deltaTime);
+
+
+		float scaledHorizontalTilt = Input.GetAxis("Horizontal") * maxHorizontalAngle;
+
+		Quaternion targetZRotation = Quaternion.Euler(mainCamera.rotation.eulerAngles.x, mainCamera.rotation.eulerAngles.y, scaledHorizontalTilt);
+
+		mainCamera.rotation = Quaternion.RotateTowards(mainCamera.rotation, targetZRotation, tiltSpeed * Time.deltaTime);
+  }
+
+	void FollowTarget()
+  {
+		Vector3 vectorA = new Vector3(transform.forward.x, 0.0f, transform.forward.z);
+
+		Vector3 vectorB = new Vector3(player.rb.velocity.x, 0.0f, player.rb.velocity.z);
+
+		float rotateAngle = Vector3.SignedAngle(vectorA.normalized, vectorB.normalized, Vector3.up);
+
+		float speedFactor = Vector3.Dot(vectorA, vectorB) > 0.0f ? vectorB.magnitude : 1.0f;
+
+		transform.Rotate(Vector3.up, rotateAngle * Mathf.Clamp(speedFactor, 1.0f, 2.0f) * Time.deltaTime);
+
+		transform.position = player.transform.position - (transform.forward * offset);
+		transform.LookAt(player.transform.position);
+  }
 }
--- Assets/Scripts/GameScripts/Player/PlayerController.cs
+++ Assets/Scripts/GameScripts/Player/PlayerController.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour 
 {	
@@ -16,36 +16,62 @@ public class PlayerController : MonoBehaviour
 	public float speed;
   public Rigidbody orientation;
   public Rigidbody spawnPoint;
-  public float maxSpeed;
+	public Rigidbody rb;
+	public float maxSpeed;
 	public STATE currentState;
+	public Vector3 floorNormal;
 
-	private Rigidbody rb;
-
+	
+	private LayerMask whatIsGround;
+	private float groundCheckRadius;
+	private float moveForce;
 	void Start()
 	{
 		rb = gameObject.GetComponent<Rigidbody> ();
 
 		currentState = STATE.WAIT;
 	}
-	
-	void Update ()
-	{
 
-    float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
-		
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		
-	  rb.AddForce (movement * speed * Time.deltaTime);
-
-    if(rb.velocity.magnitude > maxSpeed)
+  void FixedUpdate()
+  {
+		Debug.Log("Player Current State: " + currentState);
+		switch (currentState)
     {
-      rb.velocity = rb.velocity.normalized * maxSpeed;
+			case STATE.VICTORY:
+				break;
     }
+	}
 
-    orientation.transform.position = rb.position;
+  private void Update()
+  {
+		
   }
-	
+
+  public void Move(float verticalTilt, float horizontalTilt, Vector3 right)
+  {
+		if (OnGround())
+    {
+			CalculateFloorNormal();
+
+			if (horizontalTilt == 0.0f && verticalTilt == 0.0f && rb.velocity.magnitude > 0.0f)
+      {
+				rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, moveForce * 0.1f * Time.deltaTime);
+      }
+      else
+      {
+				Vector3 forward = Vector3.Cross(right, floorNormal);
+
+				Vector3 forwardForce = (verticalTilt > 0.0f ? 1.0f : 0.5f) * moveForce * verticalTilt * forward;
+
+				Vector3 rightForce = moveForce * horizontalTilt * right;
+
+				Vector3 forceVector = forwardForce + rightForce;
+
+				rb.AddForce(forceVector);
+      }
+    }
+  }
+  
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.tag == "PickUp")
@@ -64,6 +90,34 @@ public class PlayerController : MonoBehaviour
 
 	public bool OnGround()
   {
-		return true; //Physics.CheckSphere(transform.position - (Vector3.up * 0.5f), groundCheckRadius, whatIsGround)
+	  return Physics.CheckSphere(transform.position - (Vector3.up * 0.5f), groundCheckRadius, whatIsGround);
   }
+
+	private void CalculateFloorNormal()
+  {
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, whatIsGround))
+    {
+			floorNormal = hit.normal;
+    }
+  }
+	
+	/*void Update()
+	{
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed * Time.deltaTime);
+
+		if (rb.velocity.magnitude > maxSpeed)
+		{
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+		}
+
+		orientation.transform.position = rb.position;
+	}*/
 }
