74ee7d27638fe2008dde95005f4553f05cc167e0
Mustache256 <71705907+Mustache256@users.noreply.github.com>
2022-01-05 16:57:43 +0000

    Continued work on tutorial level creation and created a system for displaying tutorial messages via canvas UI

---
+++ Assets/Scripts/GameScripts/TutorialScripts/MovementTutorialScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTutorialScript : MonoBehaviour
+{
+  public GameObject MovementTutorialCanvas;
+  private void OnTriggerEnter(Collider other)
+  {
+    if(other.gameObject.tag == "Player")
+    {
+      MovementTutorialCanvas.SetActive(true);
+      Time.timeScale = 0f;
+    }
+  }
+  public void CloseTutorial()
+  {
+    MovementTutorialCanvas.SetActive(false);
+    Time.timeScale = 1f;
+  }
+}
